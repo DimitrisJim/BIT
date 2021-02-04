1f6d75f1951541b210ba1dfb072b2cf19bbd49a2
jfh <jfh@imijmi.eu>
2021-02-04 23:09:01 +0200

    Add to_bit and Resources.

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # BIT
 
 Binary Index Tree.
+
+## Resources:
+
+A couple of useful resourses for anyone looking for more:
+
+ - [tweakblogs entry][tweakblogs] is what I would recomment. After
+   understanding the intermediate representation the code becomes
+   as clear as day.
+ - [This stackexchange question][seIntuition] also has a very nice
+   description. It mainly talks about the 'a-ha' moment behind 
+   Fenwick Trees.
+ - [TopCoder article][topCoderFen], probably something most people
+   have read.
+ - [Wikipedia entry for Fenwick trees][wikiFenwick], always good to
+   give wiki a peak.
+
+[tweakblogs]: https://notes.tweakblogs.net/blog/9835/fenwick-trees-demystified.html
+[seIntuition]: https://cs.stackexchange.com/questions/10538/bit-what-is-the-intuition-behind-a-binary-indexed-tree-and-how-was-it-thought-a
+[topCoderFen]: https://www.topcoder.com/community/competitive-programming/tutorials/binary-indexed-trees/
+[wikiFenwick]: https://en.wikipedia.org/wiki/Fenwick_tree
+[algorithmistFenwick]: https://algorithmist.com/wiki/Fenwick_tree
--- bit.py
+++ bit.py
@@ -4,22 +4,55 @@ from collections.abc import MutableSequence
 
 
 class BIT(MutableSequence):
-    """ Binary Indexed Tree. """
+    """ Binary Indexed Tree.
 
-    def __init__(self, a=None, op=add):
-        pass
+
+    Notes:
+
+     - MutableSequence.__iter__ uses __getitem__. We *want* this,
+       using __getitem__ returns correct prefix sums. Yielding
+       directly from _storage wont.
+    """
+
+    def __init__(self, iterable=None, op=add):
+        self._st = self.to_bit(list(iterable or []), op)
 
     def __getitem__(self, index):
-        pass
+        """ Return prefix sum until k. """
 
     def __setitem__(self, index, value):
+        """ Update value for index k. """
         pass
 
     def __delitem__(self, index):
         pass
 
     def __len__(self):
-        return 0
+        return len(self._st)
 
     def insert(self, index, value):
         pass
+
+    @staticmethod
+    def to_list(arr, op=add):
+        """ Needs work. Looking at the intermediate
+        repr we can figure out how to brake this apart.
+
+        op plays a role, need it around.
+        """
+        pass
+
+    @staticmethod
+    def to_bit(arr, op=add):
+        """ Transform to fenwick (bit) representation. This
+        loop makes serious sense when the intermediate
+        representation in [tweakblogs] is understood.
+        """
+        i, length = 1, len(arr)
+        while i < length:
+            j = 2*i - 1
+            while j < length:
+                arr[j] = op(arr[j], arr[j-i])
+                j += 2 * i
+            i *= 2
+        return arr
